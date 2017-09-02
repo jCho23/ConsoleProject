b0e3648579678b08350bf58b50158f86d2293aa2
JuneCho23 <junecho.ucb@gmail.com>
2017-09-02 12:03:53 -0700

    added notes

--- ConsoleProject/Variables.cs
+++ ConsoleProject/Variables.cs
@@ -6,19 +6,32 @@ namespace ConsoleProject
     {
         static void Main(string[] args)
         {
-            byte number = 8;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'J';
-            string firstName = "June";
-            bool isWorking = true;
+            //This is how to write Variables 
+			//byte number = 8;
+			//int count = 10;
+			//float totalPrice = 20.95f;
+			//char character = 'J';
+			//string firstName = "June";
+			//bool isWorking = true;
 
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            //You can alaso write Variables like this.
+            //Notice that you can hover over var and see what the type is
+			//var number = 8;
+			//var count = 10;
+			//var totalPrice = 20.95f;
+			//var character = 'J';
+			//var firstName = "June";
+			//var isWorking = true;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(character);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(isWorking);
+
+            //Format String
+            Console.WriteLine();
 
         }
     }
