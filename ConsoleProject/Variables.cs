f2632fab0d9d3b95b7b173d212787e4e7efb3fc6
JuneCho23 <junecho.ucb@gmail.com>
2017-09-01 22:19:42 -0700

    finished basic var

---
+++ ConsoleProject/Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleProject
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            byte number = 8;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'J';
+            string firstName = "June";
+            bool isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+        }
+    }
+}
