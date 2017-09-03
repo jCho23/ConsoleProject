5055cbfa676f6d333902dc73f9f76ccde2813837
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 10:31:18 -0700

    PostFix

--- ConsoleProject/Operators.cs
+++ ConsoleProject/Operators.cs
@@ -3,6 +3,12 @@ namespace ConsoleProject
 {
     //CC# has 5 types of Operators
     //Arithmetic
+            //Working with numbers
+            //Increment and Decrement
+                //POSTFIX
+                    //int a = 1;
+                    //int b - a++;
+                    // Here a = 2, b= 1
     //Comparison
     //Assignment
     //Logical
