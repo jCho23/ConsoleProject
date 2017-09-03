3efd95c277a6069f33e02ad1c15b288ee72d733f
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 11:29:32 -0700

    Added Regions in Classes

--- ConsoleProject/DeclaringClasses.cs
+++ ConsoleProject/DeclaringClasses.cs
@@ -4,15 +4,34 @@ namespace ConsoleProject
 {
     public class DeclaringClasses
     {
+        #region CreatingObjects
         //creating Objects
         //Objects are INSTANCES of Classes
-        public DeclaringClasses()
-        {
-            int number;
 
-            Person person = new Person();
-            //this is the same as above BUT cleaner
-            var person = new Person();
-        }
+        //public DeclaringClasses()
+        //{
+        //    int number;
+
+        //    Person person = new Person();
+        //    //this is the same as above BUT cleaner
+        //    var person = new Person();
+        //}
+        #endregion
+
+        #region StaticModifier
+        //Static Modifier
+        //We use Static Modifiers when we only want ONE Instance of 
+        //that object in memory
+        //class Program
+        //{
+        //    //We use Static here because we only want ONE Instance of Main
+        //    //Another Example to use Static is Date/Time
+        //    //Static also allows you to access the member of the class itself
+        //    static void Main()
+        //    {
+
+        //    }
+        //}
+        #endregion
     }
 }
