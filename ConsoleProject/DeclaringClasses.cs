e2f153f5ee3672f2df56972061deeea7c2700c03
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 11:22:44 -0700

    Finished Objects

--- ConsoleProject/DeclaringClasses.cs
+++ ConsoleProject/DeclaringClasses.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Linq.Expressions;
 namespace ConsoleProject
 {
     public class DeclaringClasses
     {
+        //creating Objects
+        //Objects are INSTANCES of Classes
         public DeclaringClasses()
         {
+            int number;
+
+            Person person = new Person();
+            //this is the same as above BUT cleaner
+            var person = new Person();
         }
     }
 }
