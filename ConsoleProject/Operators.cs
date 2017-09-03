23748cad689878c840d74fa91da1e9aa0596520b
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 10:51:10 -0700

    finished basic Arithmetic

--- ConsoleProject/Operators.cs
+++ ConsoleProject/Operators.cs
@@ -2,13 +2,14 @@
 namespace ConsoleProject
 {
     //CC# has 5 types of Operators
+
     //Arithmetic
         //Working with numbers
         //Increment and Decrement
             //POSTFIX
                 //int a = 1;
-                    //int b - a++;
-                    // Here a = 2, b= 1
+                //int b - a++;
+                // Here a = 2, b= 1
     //Comparison
     //Assignment
     //Logical
@@ -16,11 +17,22 @@ namespace ConsoleProject
         //Often used in Conditional Statements
     //Bitwise
         //Used in low-level programming
-            //
+            //Windows APIs
+            //Sockets
+       
     public class Operators
     {
-        public Operators()
+        static void Main(string[] args)
         {
+            //Base Aritmatic
+            //var a = 10;
+            //var b = 3;
+            //Console.WriteLine((float)a / (float)b);
+
+            //Operator Presedence
+
+
+
         }
     }
 }
