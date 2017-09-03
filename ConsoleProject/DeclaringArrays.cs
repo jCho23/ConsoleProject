3e8848b06ee4dcfde235c52917e8f44b76f84475
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 15:27:06 -0700

    finished DeclaringArrays

--- ConsoleProject/DeclaringArrays.cs
+++ ConsoleProject/DeclaringArrays.cs
@@ -3,8 +3,16 @@ namespace ConsoleProject
 {
     public class DeclaringArrays
     {
-        public DeclaringArrays()
-        {
-        }
+        int number1;
+        int number2;
+        int number3;
+
+        //This array is the same as above 
+        // [] This tells the complier to declare an Arrary
+        // [3] Is the size of the array, thus Arrays have a fixed size
+        //Array is an Object
+        int[] numbers = new int[3];
     }
+
+
 }
