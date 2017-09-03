73593fe3f29f69914cb823dd35a49258bcc2a7ea
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 16:37:16 -0700

    bool example

--- ConsoleProject/DeclaringArrays.cs
+++ ConsoleProject/DeclaringArrays.cs
@@ -18,9 +18,34 @@ namespace ConsoleProject
     #endregion
 
     #region AccessingArrays
-    public class AccessingArrays
-    {
-        int[] numbers = new int[3] { 1, 2, 3 };
-    }
+    //public class AccessingArrays
+    //{
+    //    int[] numbers = new int[3] { 1, 2, 3 };
+    //}
     #endregion
+
+    //Array Demo
+    class ArrayDemo
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            //When you declare an Array all the elements are set to the default 
+            //value of the Data Type of that Array  
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+
+            var flags = new bool[3];
+            flags[0] = true; 
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+        }
+
+    }
 }
--- ConsoleProject/DeclaringClasses.cs
+++ ConsoleProject/DeclaringClasses.cs
@@ -1,66 +1,66 @@
-﻿using System;
-using System.Linq.Expressions;
+﻿//using System;
+//using System.Linq.Expressions;
 
-namespace ConsoleProject
-{
-    //We created a new class here
-    public class Person
-    {
-        //Here we are delaring a couple of Fields
-        public string FirstName;
-        public string LastName;
+//namespace ConsoleProject
+//{
+//    //We created a new class here
+//    public class Person
+//    {
+//        //Here we are delaring a couple of Fields
+//        public string FirstName;
+//        public string LastName;
 
-        //Here we created a Method
-        public void Introduce()
-        {
-            Console.WriteLine("My name is" + FirstName + " " + LastName);
-        }
-    }
+//        //Here we created a Method
+//        public void Introduce()
+//        {
+//            Console.WriteLine("My name is" + FirstName + " " + LastName);
+//        }
+//    }
 
-    class DeclaringClasses
-    {
-        #region CreatingObjects
-        //creating Objects
-        //Objects are INSTANCES of Classes
+//    class DeclaringClasses
+//    {
+//        #region CreatingObjects
+//        //creating Objects
+//        //Objects are INSTANCES of Classes
 
-        //public DeclaringClasses()
-        //{
-        //    int number;
+//        //public DeclaringClasses()
+//        //{
+//        //    int number;
 
-        //    Person person = new Person();
-        //    //this is the same as above BUT cleaner
-        //    var person = new Person();
-        //}
-        #endregion
+//        //    Person person = new Person();
+//        //    //this is the same as above BUT cleaner
+//        //    var person = new Person();
+//        //}
+//        #endregion
 
-        #region StaticModifier
-        //Static Modifier
-        //We use Static Modifiers when we only want ONE Instance of 
-        //that object in memory
-        //class Program
-        //{
-        //    //We use Static here because we only want ONE Instance of Main
-        //    //Another Example to use Static is Date/Time
-        //    //Static also allows you to access the member of the class itself
-        //    static void Main()
-        //    {
+//        #region StaticModifier
+//        //Static Modifier
+//        //We use Static Modifiers when we only want ONE Instance of 
+//        //that object in memory
+//        //class Program
+//        //{
+//        //    //We use Static here because we only want ONE Instance of Main
+//        //    //Another Example to use Static is Date/Time
+//        //    //Static also allows you to access the member of the class itself
+//        //    static void Main()
+//        //    {
 
-        //    }
-        //}
-        #endregion
+//        //    }
+//        //}
+//        #endregion
 
-        static void Main (string[] args)
-        {
-            //Now we are creating a OBJECT of Type PERSON
-            var june = new Person();
-            june.FirstName = "June";
-            june.LastName = "Cho";
-            june.Introduce();
+//        static void Main (string[] args)
+//        {
+//            //Now we are creating a OBJECT of Type PERSON
+//            var june = new Person();
+//            june.FirstName = "June";
+//            june.LastName = "Cho";
+//            june.Introduce();
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+//            Calculator calculator = new Calculator();
+//            var result = calculator.Add(1, 2);
+//            Console.WriteLine(result);
 
-        }
-    }
-}
+//        }
+//    }
+//}
