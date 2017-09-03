b401dae3801830ed605a11e07caaec8687f34dde
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 16:40:34 -0700

    finished 2nd Bool Example

--- ConsoleProject/DeclaringArrays.cs
+++ ConsoleProject/DeclaringArrays.cs
@@ -29,22 +29,24 @@ namespace ConsoleProject
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
+            //var numbers = new int[3];
+            //numbers[0] = 1;
 
-            //When you declare an Array all the elements are set to the default 
+            //When you declare an Array all the elements are set to the DEFAULT 
             //value of the Data Type of that Array  
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            //Console.WriteLine(numbers[0]);
+            //Console.WriteLine(numbers[1]);
+            //Console.WriteLine(numbers[2]);
 
+            //Bool Example
+            //var flags = new bool[3];
+            //flags[0] = true; 
 
-            var flags = new bool[3];
-            flags[0] = true; 
-
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
+            //Notice that Only the first Index[0] is the only one true
+            //The other two are set to their DEFAULT value of false
+            //Console.WriteLine(flags[0]);
+            //Console.WriteLine(flags[1]);
+            //Console.WriteLine(flags[2]);
         }
 
     }
