9ffaeee4f03069cf819f008a25252f5e8983eace
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 17:48:13 -0700

    created Using String Join

--- ConsoleProject/Strings.cs
+++ ConsoleProject/Strings.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 namespace ConsoleProject
 {
     public class Strings
     {
-        string firstName = "June";
+        //string firstName = "June";
 
         //StringFormat
         //Placeholders are indicated by curly braces {}
-        string name = string.Format("{0} {1}", firstName, lastName);
+        //string name = string.Format("{0} {1}", firstName, lastName);
+
+        //Using String Join
+        var numbers = new int[3] { 1, 2, 3 };
+        //First arg
+        string list = string.Join(",", numbers);
+
     }
 }
