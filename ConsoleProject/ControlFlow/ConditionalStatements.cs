1ec588e2fcdb698fbbe42a5450eabc5912916c07
JuneCho23 <junecho.ucb@gmail.com>
2017-09-04 15:43:22 -0700

    finished //SWITCH/CASE notes

--- ConsoleProject/ControlFlow/ConditionalStatements.cs
+++ ConsoleProject/ControlFlow/ConditionalStatements.cs
@@ -7,6 +7,24 @@ namespace ConsoleProject.ControlFlow
 //elseif(another Condition)
 //else(yet another Condition)
 
+//SWITCH/CASE
+//Here "role" is the variable
+//"role" is being compared to Role.Admin, Role.Moderator
+//if none of these conditions are true then we go to "default"
+
+//switch(role)
+//{
+//    case Role.Admin:
+//             ...
+//             break
+//    case Role.Moderator:
+//             ...
+//             break;
+//    default:
+//             ...
+//             break;
+//}
+
 {
     public class ConditionalStatements
     {
