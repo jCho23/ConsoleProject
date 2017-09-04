6cf87480489074b6d27589f56310e157301a5365
JuneCho23 <junecho.ucb@gmail.com>
2017-09-04 14:29:30 -0700

    finished notes

--- ConsoleProject/ReferenceTypesAndValueTypes.cs
+++ ConsoleProject/ReferenceTypesAndValueTypes.cs
@@ -11,10 +11,12 @@ namespace ConsoleProject
 
     //Classes (Reference Types):
         //You need to allocate memory youself! ( new operator)
-        //Memory allocated on HEAP (more sustainable then Stack)
-            //Arrays
-            //Strings
-            //Custom classes
+        //Memory allocated on HEAP (more sustainable then Stack) won't be removed immediately
+        //Garbage collected by CLR that takes care of the HEAP
+            //Once in a while, CLR looks at the objects that are no longer used and removes them
+                //Arrays
+                //Strings
+                //Custom classes
 
 {
     public class ReferenceTypesAndValueTypes
