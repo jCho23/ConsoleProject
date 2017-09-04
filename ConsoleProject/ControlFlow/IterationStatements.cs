84e1a62e3c1f1fdd99439c24f434fc007a838d38
JuneCho23 <junecho.ucb@gmail.com>
2017-09-04 16:50:31 -0700

    finished //ForLoops

--- ConsoleProject/ControlFlow/IterationStatements.cs
+++ ConsoleProject/ControlFlow/IterationStatements.cs
@@ -1,4 +1,17 @@
 ﻿using System;
+
+//For Loops
+//Have 3 parts: 
+//Initization Clause: How many times the loop goes 
+//Condition Clause: Loop will keep going as long as condition is still valid
+//Iteration Clause: Increment or Decrement the Counter 
+
+
+//Foreach Loops
+//While Loops
+//Do-While Loops
+
+
 namespace ConsoleProject.ControlFlow
 {
     public class IterationStatements
