5d9a628381f470b79da5120ff21c80e46a97eed3
JuneCho23 <junecho.ucb@gmail.com>
2017-09-03 17:50:36 -0700

    created regions

--- ConsoleProject/Strings.cs
+++ ConsoleProject/Strings.cs
@@ -4,17 +4,20 @@ namespace ConsoleProject
 {
     public class Strings
     {
+        #region StringFormat
         //string firstName = "June";
 
         //StringFormat
         //Placeholders are indicated by curly braces {}
         //string name = string.Format("{0} {1}", firstName, lastName);
+        #endregion
 
+        #region Using StringJoin
         //Using String Join
         //var numbers = new int[3] { 1, 2, 3 };
         //First argument is a sepeator
         //Second argument is the array that we want to combine
         //string list = string.Join(",", numbers);
-
+        #endregion
     }
 }
