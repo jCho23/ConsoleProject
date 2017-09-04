04ab3c9df2f1fad1612fbada00b30c98beb6bb4e
JuneCho23 <junecho.ucb@gmail.com>
2017-09-04 14:23:39 -0700

    2 different types

--- ConsoleProject/ReferenceTypesAndValueTypes.cs
+++ ConsoleProject/ReferenceTypesAndValueTypes.cs
@@ -1,5 +1,16 @@
 ﻿using System;
 namespace ConsoleProject
+
+//Two different Types
+    //Structures:
+        //Primitive
+            //Small, lightweight
+        //Custom Structures
+    //Classes:
+        //Arrays
+        //Strings
+        //Custom classes
+
 {
     public class ReferenceTypesAndValueTypes
     {
