1779c9c7aaa4ab45f4b2a4fa6cf8792bbb44ad0f
JuneCho23 <junecho.ucb@gmail.com>
2017-09-04 16:37:33 -0700

    created IterationStatements

--- ConsoleProject/ControlFlow/ConditionalStatements.cs
+++ ConsoleProject/ControlFlow/ConditionalStatements.cs
@@ -1,113 +1,115 @@
-﻿using System;
-namespace ConsoleProject.ControlFlow
+﻿////using System;
+////namespace ConsoleProject.ControlFlow
 
-//IF/ELSE
+////IF/ELSE
 
-//if (condition)
-//elseif(another Condition)
-//else(yet another Condition)
+////if (condition)
+////elseif(another Condition)
+////else(yet another Condition)
 
-//SWITCH/CASE
-//Here "role" is the variable
-//"role" is being compared to Role.Admin, Role.Moderator
-//if none of these conditions are true then we go to "default"
+////SWITCH/CASE
+////Here "role" is the variable
+////"role" is being compared to Role.Admin, Role.Moderator
+////if none of these conditions are true then we go to "default"
 
-//switch(role)
-//{
-//    case Role.Admin:
-//             ...
-//             break
-//    case Role.Moderator:
-//             ...
-//             break;
-//    default:
-//             ...
-//             break;
-//}
-{
-    #region ConditionalStatements
-    //class ConditionalStatements
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        int hour = 10;
+////switch(role)
+////{
+////    case Role.Admin:
+////             ...
+////             break
+////    case Role.Moderator:
+////             ...
+////             break;
+////    default:
+////             ...
+////             break;
+////}
+////{
+//    #region ConditionalStatements
+//    //class ConditionalStatements
+//    //{
+//    //    static void Main(string[] args)
+//    //    {
+//    //        int hour = 10;
 
-    //        //if Hour is greater than 0 AND less than 12...
-    //        //CW "It's morning"
-    //        if (hour > 0 && hour < 12)
-    //        {
-    //            Console.WriteLine("It's morning");
-    //        }
-    //        else if (hour >= 12 && hour < 18)
-    //        {
-    //            Console.WriteLine("It's afternoon");
-    //        }
-    //        else
-    //        {
-    //            Console.WriteLine("It's night");
-    //        }
-    //    }
-    //}
-    #endregion
+//    //        //if Hour is greater than 0 AND less than 12...
+//    //        //CW "It's morning"
+//    //        if (hour > 0 && hour < 12)
+//    //        {
+//    //            Console.WriteLine("It's morning");
+//    //        }
+//    //        else if (hour >= 12 && hour < 18)
+//    //        {
+//    //            Console.WriteLine("It's afternoon");
+//    //        }
+//    //        else
+//    //        {
+//    //            Console.WriteLine("It's night");
+//    //        }
+//    //    }
+//    //}
+//    #endregion
 
-    #region ConditionalOperators
-    //class ConditionalOperators
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        bool isGoldCustomer = true;
+//    #region ConditionalOperators
+//    //class ConditionalOperators
+//    //{
+//    //    static void Main(string[] args)
+//    //    {
+//    //        bool isGoldCustomer = true;
 
-    //        //float price;
+//    //        //float price;
 
-    //        //if (isGoldCustomer)
-    //        //{
-    //        //    price = 19.95f;
-    //        //}
-    //        //else
-    //        //{
-    //        //    price = 29.95f;
-    //        //}
+//    //        //if (isGoldCustomer)
+//    //        //{
+//    //        //    price = 19.95f;
+//    //        //}
+//    //        //else
+//    //        //{
+//    //        //    price = 29.95f;
+//    //        //}
 
-    //        //ConditionalStatement
-    //        //Notice that ":" symbolizes OR
-    //        float price = (isGoldCustomer) ? 19.95f : 29.95f;
-    //        Console.WriteLine(price);
-    //    }
-    //}
-    #endregion
+//    //        //ConditionalStatement
+//    //        //Notice that ":" symbolizes OR
+//    //        float price = (isGoldCustomer) ? 19.95f : 29.95f;
+//    //        Console.WriteLine(price);
+//    //    }
+//    //}
+//    #endregion
 
-    public enum Seasons
-    {
-        Spring,
-        Winter,
-        Fall,
-        Summer
-    }
+//    #region SwitchCases
+//    //public enum Seasons
+//    //{
+//    //    Spring,
+//    //    Winter,
+//    //    Fall,
+//    //    Summer
+//    //}
 
-    class SwitchCases
-    {
-        static void Main(string[] args)
-        {
-            var seasons = Seasons.Summer;
+//    //class SwitchCases
+//    //{
+//    //    static void Main(string[] args)
+//    //    {
+//    //        var seasons = Seasons.Summer;
 
-            switch (seasons)
-            {
-                case Seasons.Summer:
-                    Console.WriteLine("Catch some waves and daze!");
-                    break;
+//    //        switch (seasons)
+//    //        {
+//    //            case Seasons.Summer:
+//    //                Console.WriteLine("Catch some waves and daze!");
+//    //                break;
 
-                case Seasons.Fall:
-                    Console.WriteLine("Beautiful time to see the changes!");
-                    break;
+//    //            case Seasons.Fall:
+//    //                Console.WriteLine("Beautiful time to see the changes!");
+//    //                break;
 
-                case Seasons.Winter:
-                    Console.WriteLine("Burrrr, it's chilly out there!");
-                    break;
+//    //            case Seasons.Winter:
+//    //                Console.WriteLine("Burrrr, it's chilly out there!");
+//    //                break;
 
-                default:
-                    Console.WriteLine("Not a valid season");
-                    break;
-            }
-        }
-    }
-}
+//    //            default:
+//    //                Console.WriteLine("Not a valid season");
+//    //                break;
+//    //        }
+//    //    }
+//    //}
+//    //#endregion
+////}
