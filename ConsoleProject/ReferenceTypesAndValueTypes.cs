736f570e7d7b25bf396bc764f923ea41f733a6e3
JuneCho23 <junecho.ucb@gmail.com>
2017-09-04 14:40:36 -0700

    CleanUp

--- ConsoleProject/ReferenceTypesAndValueTypes.cs
+++ ConsoleProject/ReferenceTypesAndValueTypes.cs
@@ -21,8 +21,17 @@ namespace ConsoleProject
 {
     public class ReferenceTypesAndValueTypes
     {
-        public ReferenceTypesAndValueTypes()
+        static void Main(string[] args)
         {
+            var a = 10;
+            var b = a;
+            b++;
+            //When you copy a Value type to s different variable, 
+            //a COPY of that Value is taken and stored in the targer variable
+            //Thus in this case, a is 10
+            //When value types are COPIED, they become independent
+            //Thus when you change something in "a" it won't translate to "b"
+            Console.WriteLine(string.Format("a: {0}, b: {1}",a,b));
         }
     }
 }
