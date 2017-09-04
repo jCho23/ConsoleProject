c9f15677b1b1c74fe260fe99faa6accc41c0a909
JuneCho23 <junecho.ucb@gmail.com>
2017-09-04 16:08:10 -0700

    added Regions

--- ConsoleProject/ControlFlow/ConditionalStatements.cs
+++ ConsoleProject/ControlFlow/ConditionalStatements.cs
@@ -24,30 +24,31 @@ namespace ConsoleProject.ControlFlow
 //             ...
 //             break;
 //}
-
 {
-    class ConditionalStatements
-    {
-        static void Main(string[] args)
-        {
-            int hour = 10;
+    #region ConditionalStatements
+    //class ConditionalStatements
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int hour = 10;
 
-            //if Hour is greater than 0 AND less than 12...
-            //CW "It's morning"
-            if (hour > 0 && hour < 12)
-            {
-                Console.WriteLine("It's morning");
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-                Console.WriteLine("It's afternoon");
-            }
-            else
-            {
-                Console.WriteLine("It's night");
-            }
-        }
-    }
+    //        //if Hour is greater than 0 AND less than 12...
+    //        //CW "It's morning"
+    //        if (hour > 0 && hour < 12)
+    //        {
+    //            Console.WriteLine("It's morning");
+    //        }
+    //        else if (hour >= 12 && hour < 18)
+    //        {
+    //            Console.WriteLine("It's afternoon");
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("It's night");
+    //        }
+    //    }
+    //}
+    #endregion
 
     class ConditionalOperators
     {
