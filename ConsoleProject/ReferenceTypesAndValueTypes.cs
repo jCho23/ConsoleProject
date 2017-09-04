cf4624a7a0b2ac22847e95bb27586858cfa492e7
JuneCho23 <junecho.ucb@gmail.com>
2017-09-04 14:26:55 -0700

    Reference Types

--- ConsoleProject/ReferenceTypesAndValueTypes.cs
+++ ConsoleProject/ReferenceTypesAndValueTypes.cs
@@ -2,15 +2,19 @@
 namespace ConsoleProject
 
 //Two different Types
+
     //Structures (Value Types):
-    //Allocated on Stack, Memory management done automatically
-        //Primitive
-            //Small, lightweight
-        //Custom Structures
+        //Allocated on Stack, Memory management done automatically
+            //Primitive
+                //Small, lightweight
+            //Custom Structures
+
     //Classes (Reference Types):
-        //Arrays
-        //Strings
-        //Custom classes
+        //You need to allocate memory youself! ( new operator)
+        //Memory allocated on HEAP (more sustainable then Stack)
+            //Arrays
+            //Strings
+            //Custom classes
 
 {
     public class ReferenceTypesAndValueTypes
