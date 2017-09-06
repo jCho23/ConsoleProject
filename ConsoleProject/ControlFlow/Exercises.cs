77b6b61f65e972da143806716aa0279ea51ffa57
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 19:11:18 -0700

    finished Exercise4

--- ConsoleProject/ControlFlow/Exercises.cs
+++ ConsoleProject/ControlFlow/Exercises.cs
@@ -64,5 +64,28 @@ namespace ConsoleProject.ControlFlow
         //Give the user 4 chances to guess
         //If the user guesses the number, "You WON!!!"
         //If not, "You LOST"
+
+        static void Main(string[] args)
+        {
+            var number = new Random().Next(1, 10);
+
+            Console.WriteLine("Secret is " + number);
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Guess the secret number: ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You WON!!!");
+                    return;
+                }
+
+                else
+                {
+                    Console.WriteLine("You LOST");
+                }
+            }
+        }
     }
 }
