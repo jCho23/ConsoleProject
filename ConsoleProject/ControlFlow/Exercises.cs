75342e31b68744c6c565cd71ca7bdee82fe55b93
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 18:02:08 -0700

    Exercise1

--- ConsoleProject/ControlFlow/Exercises.cs
+++ ConsoleProject/ControlFlow/Exercises.cs
@@ -3,8 +3,14 @@ namespace ConsoleProject.ControlFlow
 {
     public class Exercises
     {
-        public Exercises()
+        //Create a program that counts how many numbers between 1 and 100
+        //Divisible by 3 with no remainder
+        //Display results on Console
+
+        public void Exercise1()
         {
+
+
         }
     }
 }
