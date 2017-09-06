6a0bf35d0513781d46b961723ac030f3308bbc46
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 18:07:29 -0700

    finished first exercise

--- ConsoleProject/ControlFlow/Exercises.cs
+++ ConsoleProject/ControlFlow/Exercises.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 namespace ConsoleProject.ControlFlow
 {
-    public class Exercises
+    class Exercises
     {
         //Create a program that counts how many numbers between 1 and 100
         //Divisible by 3 with no remainder
         //Display results on Console
 
-        public void Exercise1()
+        static void Main(string[] args)
         {
-
+            var count = 0;
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    count++;
+            }
+            Console.WriteLine("There are {0} numbers divisible by 3 between 1 and 100", count);
 
         }
     }
