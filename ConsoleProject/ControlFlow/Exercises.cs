f60879ae8d69e78a300aa80dbf82decee4e50de5
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 18:42:04 -0700

    finished exercise2

--- ConsoleProject/ControlFlow/Exercises.cs
+++ ConsoleProject/ControlFlow/Exercises.cs
@@ -21,13 +21,25 @@ namespace ConsoleProject.ControlFlow
         //}
         #endregion
 
+         
         //Create a program that continuosly asks the user to enter a number
         //The loop terminates when the user enter "OK"
         //Calculate the sum of all the previouslt entered numbers and display it 
 
         static void Main(string[] args)
         {
-            
+            var sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter a number(or 'OK' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "OK")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("Sum of all numbers is: " + sum);
         }
     }
 }
