858a325162b601fa5b3030647f17ea74a5e48942
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 20:22:11 -0700

    finished IndexOf

--- ConsoleProject/ArraysAndLists/Arrays.cs
+++ ConsoleProject/ArraysAndLists/Arrays.cs
@@ -15,6 +15,12 @@ namespace ConsoleProject.ArraysAndLists
 
             //Length: Returns the size of the Array
             Console.WriteLine("Length: " + numbers.Length);
+
+            //IndexOf(): This is the IndexOf Method 
+            //This method finds the positions of an element in the Array
+            var index= Array.IndexOf(numbers, 9);
+            //Index of 9 is 2, since it's the third nubmer
+            Console.WriteLine("Index of 9: " + index);
         }
     }
 }
