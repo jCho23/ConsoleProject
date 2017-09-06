20c50b88035811eb0130b41ad1b36aa41919ce46
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 18:09:04 -0700

    added regions

--- ConsoleProject/ControlFlow/Exercises.cs
+++ ConsoleProject/ControlFlow/Exercises.cs
@@ -3,20 +3,24 @@ namespace ConsoleProject.ControlFlow
 {
     class Exercises
     {
+        #region ExerciseOne
         //Create a program that counts how many numbers between 1 and 100
         //Divisible by 3 with no remainder
         //Display results on Console
 
-        static void Main(string[] args)
-        {
-            var count = 0;
-            for (var i = 1; i <= 100; i++)
-            {
-                if (i % 3 == 0)
-                    count++;
-            }
-            Console.WriteLine("There are {0} numbers divisible by 3 between 1 and 100", count);
+        //static void Main(string[] args)
+        //{
+        //    var count = 0;
+        //    for (var i = 1; i <= 100; i++)
+        //    {
+        //        if (i % 3 == 0)
+        //            count++;
+        //    }
+        //    Console.WriteLine("There are {0} numbers divisible by 3 between 1 and 100", count);
 
-        }
+        //}
+        #endregion
+
+
     }
 }
