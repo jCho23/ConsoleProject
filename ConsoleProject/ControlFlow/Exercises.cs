4bd7b2e571f209a264f5d6fe4b322f3f4f49d0d4
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 19:12:05 -0700

    Regions and CleanUp

--- ConsoleProject/ControlFlow/Exercises.cs
+++ ConsoleProject/ControlFlow/Exercises.cs
@@ -60,32 +60,34 @@ namespace ConsoleProject.ControlFlow
         //}
         #endregion
 
+        #region Exercise4
         //Create a program that picks a random number between 1 and 10
         //Give the user 4 chances to guess
         //If the user guesses the number, "You WON!!!"
         //If not, "You LOST"
 
-        static void Main(string[] args)
-        {
-            var number = new Random().Next(1, 10);
+        //static void Main(string[] args)
+        //{
+        //    var number = new Random().Next(1, 10);
 
-            Console.WriteLine("Secret is " + number);
-            for (var i = 0; i < 4; i++)
-            {
-                Console.WriteLine("Guess the secret number: ");
-                var guess = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Secret is " + number);
+        //    for (var i = 0; i < 4; i++)
+        //    {
+        //        Console.WriteLine("Guess the secret number: ");
+        //        var guess = Convert.ToInt32(Console.ReadLine());
 
-                if (guess == number)
-                {
-                    Console.WriteLine("You WON!!!");
-                    return;
-                }
+        //        if (guess == number)
+        //        {
+        //            Console.WriteLine("You WON!!!");
+        //            return;
+        //        }
 
-                else
-                {
-                    Console.WriteLine("You LOST");
-                }
-            }
-        }
+        //        else
+        //        {
+        //            Console.WriteLine("You LOST");
+        //        }
+        //    }
+        //}
+        #endregion
     }
 }
