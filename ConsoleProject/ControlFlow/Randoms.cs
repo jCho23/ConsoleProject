ef08098b7215be0166d39653cd21405d1cb61b76
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 17:07:20 -0700

    CleanUp

--- ConsoleProject/ControlFlow/Randoms.cs
+++ ConsoleProject/ControlFlow/Randoms.cs
@@ -1,34 +1,34 @@
-﻿using System;
-namespace ConsoleProject.ControlFlow
-{
-    class Randoms
-    {
-        static void Main(string[] args)
-        {
-            //var random = new Random();
+﻿//using System;
+//namespace ConsoleProject.ControlFlow
+//{
+//    class Randoms
+//    {
+//        static void Main(string[] args)
+//        {
+//            //var random = new Random();
 
-            ////Here, "random.Next" has multiple types
-            //for (var i = 0; i < 10; i++)
-            ////We are CASTING "char" to get a character from the random number
-            //Console.Write((char)random.Next(97, 122));
+//            ////Here, "random.Next" has multiple types
+//            //for (var i = 0; i < 10; i++)
+//            ////We are CASTING "char" to get a character from the random number
+//            //Console.Write((char)random.Next(97, 122));
 
-            ////"0, 26" represents the 26 letters in the alphabet
-            //Console.Write((char)('a'+random.Next(0, 26)));
+//            ////"0, 26" represents the 26 letters in the alphabet
+//            //Console.Write((char)('a'+random.Next(0, 26)));
 
-            //Storing results in Array and Creating a String from that Array
-            var random = new Random();
+//            //Storing results in Array and Creating a String from that Array
+//            var random = new Random();
 
-            //We created this constant so that we can change the value in ONE place
-            const int passwordLength = 10;
+//            //We created this constant so that we can change the value in ONE place
+//            const int passwordLength = 10;
 
-            //This is a char Array called buffer (highlight var)
-            var buffer = new char[passwordLength];
+//            //This is a char Array called buffer (highlight var)
+//            var buffer = new char[passwordLength];
 
-            for (var i = 0; i < passwordLength; i++)
-                buffer[i] = (char)('a' + random.Next(0, 26));
+//            for (var i = 0; i < passwordLength; i++)
+//                buffer[i] = (char)('a' + random.Next(0, 26));
 
-            var password = new string(buffer);
-            Console.WriteLine(password);
-        }
-    }
-}
+//            var password = new string(buffer);
+//            Console.WriteLine(password);
+//        }
+//    }
+//}
