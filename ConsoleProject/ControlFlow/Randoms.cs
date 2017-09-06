71e991c0ef4a739a0c99b47e0ca7379be2ab0672
JuneCho23 <junecho.ucb@gmail.com>
2017-09-05 17:01:46 -0700

    /Storing results in Array

--- ConsoleProject/ControlFlow/Randoms.cs
+++ ConsoleProject/ControlFlow/Randoms.cs
@@ -5,14 +5,28 @@ namespace ConsoleProject.ControlFlow
     {
         static void Main(string[] args)
         {
+            //var random = new Random();
+
+            ////Here, "random.Next" has multiple types
+            //for (var i = 0; i < 10; i++)
+            ////We are CASTING "char" to get a character from the random number
+            //Console.Write((char)random.Next(97, 122));
+
+            ////"0, 26" represents the 26 letters in the alphabet
+            //Console.Write((char)('a'+random.Next(0, 26)));
+
+            //Storing results in Array and Creating a String from that Array
             var random = new Random();
+            //This is a char Array called buffer (highlight var)
+			var buffer = new char[10];
 
-            //Here, "random.Next" has multiple types
-            for (var i = 0; i < 10; i++)
-                //We are CASTING "char" to get a character from the random number
-                Console.WriteLine((char)random.Next(97, 122));
+            for (var i = 0; i < 10; i++) 
+			Console.Write((char)('a'+random.Next(0, 26)));
 
 
-        }
+
+
+
+		}
     }
 }
