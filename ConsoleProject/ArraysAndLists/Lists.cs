c38a3724501c710e59b96257aa43341175556107
JuneCho23 <junecho.ucb@gmail.com>
2017-09-06 19:05:22 -0700

    CleanUp

--- ConsoleProject/ArraysAndLists/Lists.cs
+++ ConsoleProject/ArraysAndLists/Lists.cs
@@ -1,65 +1,65 @@
-﻿using System;
+﻿//using System;
 
-//Array: Fixed Size
-//Lists: Dynamic Size
+////Array: Fixed Size
+////Lists: Dynamic Size
 
-//Useful List Methods
-//Add()
-//AddRange()
-//Remove()
-//RemoveAt()
-//IndexOf()
-//Contains()
-//Count()
-using System.Collections.Generic;
+////Useful List Methods
+////Add()
+////AddRange()
+////Remove()
+////RemoveAt()
+////IndexOf()
+////Contains()
+////Count()
+//using System.Collections.Generic;
 
-namespace ConsoleProject.ArraysAndLists
-{
-    class Lists
-    {
-        static void Main(string[] args)
-        {
-            var numbers = new List<int>() { 1, 2, 3, 4 };
-            //Notice that in Lists you can ADD objects
-            //In Arrays, you can't add object since it is fixed
-            numbers.Add(1);
-            //When you see IEnumerable (hover AddRange), add an Array or List
-            //Capital I = interface
-            numbers.AddRange(new int[3] { 5, 6, 7 });
+//namespace ConsoleProject.ArraysAndLists
+//{
+//    class Lists
+//    {
+//        static void Main(string[] args)
+//        {
+//            var numbers = new List<int>() { 1, 2, 3, 4 };
+//            //Notice that in Lists you can ADD objects
+//            //In Arrays, you can't add object since it is fixed
+//            numbers.Add(1);
+//            //When you see IEnumerable (hover AddRange), add an Array or List
+//            //Capital I = interface
+//            numbers.AddRange(new int[3] { 5, 6, 7 });
 
-            foreach (var number in numbers)
-                Console.WriteLine(number);
+//            foreach (var number in numbers)
+//                Console.WriteLine(number);
 
-            Console.WriteLine();
-            //This starts the search from the beginning of the list
-            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
-            //This starts the search from the end of the list
-            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
+//            Console.WriteLine();
+//            //This starts the search from the beginning of the list
+//            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+//            //This starts the search from the end of the list
+//            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
 
-            //Here COUNT is a PROPERTY and Returns the number of objects in the list
-            Console.WriteLine("Count: "+ numbers.Count);
+//            //Here COUNT is a PROPERTY and Returns the number of objects in the list
+//            Console.WriteLine("Count: "+ numbers.Count);
 
-            //         numbers.Remove(1);
-            //foreach (var number in numbers)
-            //Notice that it onle removed the 1st "1"
-            //Console.WriteLine(number);
+//            //         numbers.Remove(1);
+//            //foreach (var number in numbers)
+//            //Notice that it onle removed the 1st "1"
+//            //Console.WriteLine(number);
 
 
-            //This is how you Remove All the "1's" in the list
-            //Notice that we changed to a ForLoop
-            for (var i = 0; i < numbers.Count; i++)
-            {
-                if (numbers[i] == 1)
-                    numbers.Remove(numbers[i]);
-            }
+//            //This is how you Remove All the "1's" in the list
+//            //Notice that we changed to a ForLoop
+//            for (var i = 0; i < numbers.Count; i++)
+//            {
+//                if (numbers[i] == 1)
+//                    numbers.Remove(numbers[i]);
+//            }
 
-			foreach (var number in numbers)
-				Console.WriteLine(number);
+//			foreach (var number in numbers)
+//				Console.WriteLine(number);
 
-            //This Clear Method removes ALL objects from the list
-            numbers.Clear();
-            Console.WriteLine("Count: " + numbers.Count);
+//            //This Clear Method removes ALL objects from the list
+//            numbers.Clear();
+//            Console.WriteLine("Count: " + numbers.Count);
 
-        }
-    }
-}
+//        }
+//    }
+//}
