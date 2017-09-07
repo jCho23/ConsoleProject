c69a890f1792deaec904c02c437d4b504d1c73cb
JuneCho23 <junecho.ucb@gmail.com>
2017-09-06 20:54:55 -0700

    finished demo3

--- ConsoleProject/ArraysAndLists/DemoArrayAndLists.cs
+++ ConsoleProject/ArraysAndLists/DemoArrayAndLists.cs
@@ -59,6 +59,23 @@ namespace ConsoleProject.ArraysAndLists
         static void Main(string[] args)
         {
             var numbers = new List<int>();
+            while(numbers.Count<5)
+            {
+                Console.WriteLine("Enter a number: ");
+                var number= Convert.ToInt32(Console.ReadLine());
+                if (numbers.Contains(number))
+                {
+                    Console.WriteLine("You've previously entered " + number);
+                    continue;
+                }
+
+                numbers.Add(number);
+            }
+
+            numbers.Sort();
+
+            foreach(var number in numbers)
+                Console.WriteLine(number);
         }
 
 
