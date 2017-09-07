91f18049600e063b53d050a01edcaf60e53f2554
JuneCho23 <junecho.ucb@gmail.com>
2017-09-06 20:03:16 -0700

    Exercise1

---
+++ ConsoleProject/ArraysAndLists/Exercises.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleProject.ArraysAndLists
+{
+    public class Exercises
+    {
+        //Ask the user to enter different names, until the user presses Enter (w/o name)
+        //Depending on the names provides, display a message
+
+        public void Exercise1()
+        {
+            var names = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Type a name (or hit ");
+            }
+
+        }
+    }
+}
