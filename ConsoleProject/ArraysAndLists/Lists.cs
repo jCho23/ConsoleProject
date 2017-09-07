117a0c8346749965221c3deb0f957e2e8113cac2
JuneCho23 <junecho.ucb@gmail.com>
2017-09-06 18:44:48 -0700

    How to Add objects in Lists

--- ConsoleProject/ArraysAndLists/Lists.cs
+++ ConsoleProject/ArraysAndLists/Lists.cs
@@ -19,13 +19,16 @@ namespace ConsoleProject.ArraysAndLists
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int>(){1,2,3,4};
+            var numbers = new List<int>() { 1, 2, 3, 4 };
             //Notice that in Lists you can ADD objects
             //In Arrays, you can't add object since it is fixed
             numbers.Add(1);
             //When you see IEnumerable (hover AddRange), add an Array or List
             //Capital I = interface
-            numbers.AddRange();
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
         }
     }
 }
