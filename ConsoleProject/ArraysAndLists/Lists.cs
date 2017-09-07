27d6e0225b63b0074826b18b85fad9d9d42a68ca
JuneCho23 <junecho.ucb@gmail.com>
2017-09-06 18:34:17 -0700

    created Lists

---
+++ ConsoleProject/ArraysAndLists/Lists.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//Array: Fixed Size
+//Lists: Dynamic Size
+
+
+namespace ConsoleProject.ArraysAndLists
+{
+    public class Lists
+    {
+        public Lists()
+        {
+        }
+    }
+}
