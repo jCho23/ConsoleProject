6108467f0d973e4d85e8e9c79fe2aade8ea53c30
JuneCho23 <junecho.ucb@gmail.com>
2017-09-06 18:37:13 -0700

    Useful List Methods

--- ConsoleProject/ArraysAndLists/Lists.cs
+++ ConsoleProject/ArraysAndLists/Lists.cs
@@ -3,6 +3,14 @@
 //Array: Fixed Size
 //Lists: Dynamic Size
 
+//Useful List Methods
+//Add()
+//AddRange()
+//Remove()
+//RemoveAt()
+//IndexOf()
+//Contains()
+//Count()
 
 namespace ConsoleProject.ArraysAndLists
 {
