84b27ef84ad8a8bee28ae76b45807e67bf697ae8
JuneCho23 <junecho.ucb@gmail.com>
2017-09-06 20:12:10 -0700

    finished 1st

--- ConsoleProject/ArraysAndLists/Exercises.cs
+++ ConsoleProject/ArraysAndLists/Exercises.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace ConsoleProject.ArraysAndLists
 {
@@ -7,15 +8,29 @@ namespace ConsoleProject.ArraysAndLists
     {
         //Ask the user to enter different names, until the user presses Enter (w/o name)
         //Depending on the names provides, display a message
-
-        public void Exercise1()
+        static void Main(string[] args)
         {
+
             var names = new List<string>();
             while (true)
             {
-                Console.WriteLine("Type a name (or hit ");
+                Console.WriteLine("Type a name (or hit Enter to Quit): ");
+
+                var input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+                names.Add(input);
             }
 
+            if (names.Count > 2)
+                Console.WriteLine("{0}, {1}, and {2} others like your post", 
+                                  names[0], names[1], names.Count - 2);
+            else if (names.Count == 2)
+                Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
+            else if (names.Count == 1)
+                Console.WriteLine("{0} likes your post", names[0]);
+            else
+                Console.WriteLine();
         }
     }
 }
