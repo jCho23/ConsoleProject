6cfddec54d084b22d2aaccd4a11af4692c4c9996
JuneCho23 <junecho.ucb@gmail.com>
2017-09-06 20:14:52 -0700

    cleanUp

--- ConsoleProject/ArraysAndLists/Exercises.cs
+++ ConsoleProject/ArraysAndLists/Exercises.cs
@@ -6,31 +6,34 @@ namespace ConsoleProject.ArraysAndLists
 {
     public class Exercises
     {
+        //Exeercise 1
         //Ask the user to enter different names, until the user presses Enter (w/o name)
         //Depending on the names provides, display a message
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
 
-            var names = new List<string>();
-            while (true)
-            {
-                Console.WriteLine("Type a name (or hit Enter to Quit): ");
+        //    var names = new List<string>();
+        //    while (true)
+        //    {
+        //        Console.WriteLine("Type a basketball player (or hit Enter to Quit): ");
 
-                var input = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(input))
-                    break;
-                names.Add(input);
-            }
+        //        var input = Console.ReadLine();
+        //        if (string.IsNullOrWhiteSpace(input))
+        //            break;
+        //        names.Add(input);
+        //    }
 
-            if (names.Count > 2)
-                Console.WriteLine("{0}, {1}, and {2} others like your post", 
-                                  names[0], names[1], names.Count - 2);
-            else if (names.Count == 2)
-                Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
-            else if (names.Count == 1)
-                Console.WriteLine("{0} likes your post", names[0]);
-            else
-                Console.WriteLine();
-        }
+        //    if (names.Count > 2)
+        //        Console.WriteLine("{0}, {1}, and {2} all jammed out!", 
+        //                          names[0], names[1], names.Count - 2);
+        //    else if (names.Count == 2)
+        //        Console.WriteLine("{0} and {1} dunked on you hard", names[0], names[1]);
+        //    else if (names.Count == 1)
+        //        Console.WriteLine("{0} dunked on you", names[0]);
+        //    else
+        //        Console.WriteLine();
+        //}
+
+
     }
 }
