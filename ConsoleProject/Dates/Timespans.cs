b209ff49db64a6282bc3637dc762c41532c1f0c9
JuneCho23 <junecho.ucb@gmail.com>
2017-09-07 20:33:32 -0700

    finished         //Creating TimeSpan Objects

--- ConsoleProject/Dates/Timespans.cs
+++ ConsoleProject/Dates/Timespans.cs
@@ -3,6 +3,7 @@ namespace ConsoleProject.Dates
 {
     public class Timespans
     {
+        //Creating TimeSpan Objects
         static void Main(String[] args)
         {
             //1 hour, 2 min, 3 sec
@@ -16,6 +17,11 @@ namespace ConsoleProject.Dates
             //IF you have 2 DateTime objects,
             //And you subtract them...
             //The Result is a TimeSpan
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start; 
+
+            Console.WriteLine("Duration: " + duration);
 
         }
     }
