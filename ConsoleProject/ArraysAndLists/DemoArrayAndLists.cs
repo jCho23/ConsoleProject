57109ee1150986661cf47495a779854ecf8f5cf9
JuneCho23 <junecho.ucb@gmail.com>
2017-09-07 17:48:03 -0700

    finished demo 4

--- ConsoleProject/ArraysAndLists/DemoArrayAndLists.cs
+++ ConsoleProject/ArraysAndLists/DemoArrayAndLists.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using System.Net.Http.Headers;
 
 namespace ConsoleProject.ArraysAndLists
 {
@@ -83,14 +84,32 @@ namespace ConsoleProject.ArraysAndLists
         //Ask the user to continuously enter a number or type "Quit" to exit
         //The list of numbers may include duplicates
         //Display the unique numbers that the user has entered 
-        static void Main(string[] args)
-        {
-            var numbers
+        //static void Main(string[] args)
+        //{
+        //    var numbers = new List<int>();
 
+        //    while (true)
+        //    {
+        //        Console.WriteLine("Enter a number (or 'Quit' to exit): ");
+        //        var input = Console.ReadLine();
 
+        //        if (input.ToLower() == "quit")
+        //            break;
 
+        //        numbers.Add(Convert.ToInt32(input));
+        //    }
 
-        }
+        //    var uniques = new List<int>();
+        //    foreach (var number in numbers)
+        //    {
+        //        if (!uniques.Contains(number))
+        //            uniques.Add(number);
+        //    }
+
+        //    Console.WriteLine("Unique numbers:");
+        //    foreach(var number in uniques)
+        //        Console.WriteLine(number);
+        //}
 
     }
 }
