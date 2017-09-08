db60c872460ee6b420b831e72903e47353d60b05
JuneCho23 <junecho.ucb@gmail.com>
2017-09-07 20:29:45 -0700

    TimeSpan Static Methods

--- ConsoleProject/Dates/Timespans.cs
+++ ConsoleProject/Dates/Timespans.cs
@@ -3,8 +3,15 @@ namespace ConsoleProject.Dates
 {
     public class Timespans
     {
-        public Timespans()
+        static void Main(String[] args)
         {
+            //1 hour, 2 min, 3 sec
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpanNoValue = new TimeSpan(1, 0, 0);
+
+            //To make the code above more clear,
+            //Use the static methods on the TimeSpan Structure
+            var timeSpanNoValue2= TimeSpan.FromHours(1);
         }
     }
 }
