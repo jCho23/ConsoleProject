750f8f64101a85939621833ae56e85cf1bc07f13
JuneCho23 <junecho.ucb@gmail.com>
2017-09-07 20:16:29 -0700

    DateTime properties

---
+++ ConsoleProject/Dates/Dates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ConsoleProject.Dates
+{
+    public class Dates
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2017, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+        }
+    }
+}
