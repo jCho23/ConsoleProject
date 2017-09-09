90f32ef0ec38ee3c6f6d2a34b327605657e6ce77
JuneCho23 <junecho.ucb@gmail.com>
2017-09-09 16:13:12 -0700

    Methods of StringBuilders

--- ConsoleProject/Texts/StringBuilders.cs
+++ ConsoleProject/Texts/StringBuilders.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 //StringBuilder
 //1) Defined in System.Text
@@ -6,7 +7,7 @@
 //3) Easy and Fast to create and manipulate strings
 //4) NOT for Searching
 
-//String Manipulation Methods
+//=\
 //Append()
 //Insert()
 //Remove()
@@ -17,8 +18,20 @@ namespace ConsoleProject.Texts
 {
     public class StringBuilders
     {
-        public StringBuilders()
+        static void Main(string[] args)
         {
+            var builder = new StringBuilder();
+            //Methods of StringBuilders
+            //This '-' is repeated 10 times here
+            builder.Append('-', 10);
+
+            //This will insert "Header"
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+
+            Console.WriteLine(builder);
         }
     }
 }
