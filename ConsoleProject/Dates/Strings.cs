c77bc4d724ce2e7eb8cf155ee084dcf9eb284faa
JuneCho23 <junecho.ucb@gmail.com>
2017-09-09 12:03:32 -0700

    Searching notes

--- ConsoleProject/Dates/Strings.cs
+++ ConsoleProject/Dates/Strings.cs
@@ -5,6 +5,12 @@
 //ToUpper() "HELLO WORLD"
 //Trim() Gets rid of white space around the String
 
+//Searching
+//Both these Methods have Overloads and they accept Characters and Strings
+//And they Return an Index of the First or Last character in the String
+//IndexOf('a')
+//LastIndexOf("Hello")
+
 namespace ConsoleProject.Dates
 {
     public class Strings
