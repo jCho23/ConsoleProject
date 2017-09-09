2e2687091e11c5f5a3a850ef3647367f72d36667
JuneCho23 <junecho.ucb@gmail.com>
2017-09-09 15:46:41 -0700

    String Manipulation Methods

--- ConsoleProject/Texts/StringBuilders.cs
+++ ConsoleProject/Texts/StringBuilders.cs
@@ -1,4 +1,18 @@
 ﻿using System;
+
+//StringBuilder
+//1) Defined in System.Text
+//2) A mutable String
+//3) Easy and Fast to create and manipulate strings
+//4) NOT for Searching
+
+//String Manipulation Methods
+//Append()
+//Insert()
+//Remove()
+//Replace()
+//Clear()
+
 namespace ConsoleProject.Texts
 {
     public class StringBuilders
