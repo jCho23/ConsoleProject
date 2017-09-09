ff71b9c8ec7bbc7d14fad01d0fcb827b11e85fe4
JuneCho23 <junecho.ucb@gmail.com>
2017-09-09 15:32:17 -0700

    summaryWords

---
+++ ConsoleProject/Texts/SummarisingTexts.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleProject.Texts
+{
+    class SummarisingTexts
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is an example of a super long sentence that is best exemplified in a blog post of some kind, if you know what I mean!";
+           
+            }
+        }
+
+        static string SummarizeText(string text)
+        {
+		const int maxlength = 20;
+
+        if (text.Length < maxlength)
+            return text;
+        
+		else
+		{
+			//Breaking sentences into words
+			var words = text.Split(' ');
+			var totalCharacters = 0;
+			var summaryWords = new List<string>();
+
+			foreach (var word in words)
+			{
+				summaryWords.Add(word);
+
+				totalCharacters += word.Length + 1;
+				if (totalCharacters > maxlength)
+					break;
+			}
+
+			//Note that no words are cut off since we Split them by ' '
+			//var summary = String.Join(" ", summaryWords) + "...";
+            //return summary;
+
+            //This is the same code as above
+            return String.Join(" ", summaryWords) + "...";
+        }
+    }
+}
