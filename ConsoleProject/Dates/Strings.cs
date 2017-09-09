60e22f52cd786f12da6abb4c6badeb6bcf1a1169
JuneCho23 <junecho.ucb@gmail.com>
2017-09-09 11:58:34 -0700

    Formatting notes

---
+++ ConsoleProject/Dates/Strings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Formatting
+//ToLower() "hello world"
+//ToUpper() "HELLO WORLD"
+//Trim() Gets rid of white space around the String
+
+namespace ConsoleProject.Dates
+{
+    public class Strings
+    {
+        public Strings()
+        {
+        }
+    }
+}
