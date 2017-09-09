8ea13ac492edfc819e81aa938df27c393d85d145
JuneCho23 <junecho.ucb@gmail.com>
2017-09-09 15:38:41 -0700

    CleanUp

--- ConsoleProject/Texts/SummarisingTexts.cs
+++ ConsoleProject/Texts/SummarisingTexts.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
 
-namespace ConsoleProject.Texts
-{
-    class SummarisingTexts
-    {
-        static void Main(string[] args)
-        {
-            var sentence = "This is an example of a super long sentence that is best exemplified in a blog post of some kind, if you know what I mean!";
-            var summary = SummarizeText(sentence, 30);
-            Console.WriteLine(summary);
-        }
+//namespace ConsoleProject.Texts
+//{
+//    class SummarisingTexts
+//    {
+//        static void Main(string[] args)
+//        {
+//            var sentence = "This is an example of a super long sentence that is best exemplified in a blog post of some kind, if you know what I mean!";
+//            var summary = SummarizeText(sentence, 30);
+//            Console.WriteLine(summary);
+//        }
 
-        static string SummarizeText(string text, int maxLength = 20)
-        {
-            //Instead of hardcoding the max length here, we are passing it through the Method
-            const int maxlength = 20;
+//        static string SummarizeText(string text, int maxLength = 20)
+//        {
+//            //Instead of hardcoding the max length here, we are passing it through the Method
+//            const int maxlength = 20;
 
-            if (text.Length < maxlength)
-                return text;
+//            if (text.Length < maxlength)
+//                return text;
 
-            else
-            {
-                //Breaking sentences into words
-                var words = text.Split(' ');
-                var totalCharacters = 0;
-                var summaryWords = new List<string>();
+//            else
+//            {
+//                //Breaking sentences into words
+//                var words = text.Split(' ');
+//                var totalCharacters = 0;
+//                var summaryWords = new List<string>();
 
-                foreach (var word in words)
-                {
-                    summaryWords.Add(word);
+//                foreach (var word in words)
+//                {
+//                    summaryWords.Add(word);
 
-                    totalCharacters += word.Length + 1;
-                    if (totalCharacters > maxlength)
-                        break;
-                }
+//                    totalCharacters += word.Length + 1;
+//                    if (totalCharacters > maxlength)
+//                        break;
+//                }
 
-                //Note that no words are cut off since we Split them by ' '
-                //var summary = String.Join(" ", summaryWords) + "...";
-                //return summary;
+//                //Note that no words are cut off since we Split them by ' '
+//                //var summary = String.Join(" ", summaryWords) + "...";
+//                //return summary;
 
-                //This is the same code as above
-                return String.Join(" ", summaryWords) + "...";
-            }
-        }
-    }
-}
+//                //This is the same code as above
+//                return String.Join(" ", summaryWords) + "...";
+//            }
+//        }
+//    }
+//}
