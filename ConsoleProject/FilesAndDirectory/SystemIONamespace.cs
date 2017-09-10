bd591bd756db92145179ce5fd49fd948189ccd65
JuneCho23 <junecho.ucb@gmail.com>
2017-09-10 10:54:33 -0700

    CleanUp

--- ConsoleProject/FilesAndDirectory/SystemIONamespace.cs
+++ ConsoleProject/FilesAndDirectory/SystemIONamespace.cs
@@ -13,6 +13,7 @@
 
 //Path: Provides Methods to work with a String that contains a File or Directory Path information
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace ConsoleProject.FilesAndDirectory
 {
@@ -21,34 +22,38 @@ namespace ConsoleProject.FilesAndDirectory
         //File and FileInfo
         //static void Main(string[] args)
         //{
-            ////File (Static Methods)
-            //var path = @"c:\somefile.jpg";
+        ////File (Static Methods)
+        //var path = @"c:\somefile.jpg";
 
-            //File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+        //File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
 
-            //File.Delete(path);
-            //if (File.Exists(path))
-            //{
-            //    //
-            //}
-            //var content = File.ReadAllText(path);
+        //File.Delete(path);
+        //if (File.Exists(path))
+        //{
+        //    //
+        //}
+        //var content = File.ReadAllText(path);
 
 
-            ////FileInfo (Instance Methods)
-            //var fileInfo = new FileInfo(path);
-            //fileInfo.CopyTo("...");
-            //fileInfo.Delete();
+        ////FileInfo (Instance Methods)
+        //var fileInfo = new FileInfo(path);
+        //fileInfo.CopyTo("...");
+        //fileInfo.Delete();
 
-            ////This is not a Method, this is a Property
-            //if (fileInfo.Exists)
-            //{
-            //    //
-            //}
+        ////This is not a Method, this is a Property
+        //if (fileInfo.Exists)
+        //{
+        //    //
+        //}
 
         //Directory and DirectoryPath
+
+
         static void Main(string[] args)
         {
-            
+			var path = @"c:\temp\someFile";
+
+            Directory.CreateDirectory(path);
         }
 
     }
