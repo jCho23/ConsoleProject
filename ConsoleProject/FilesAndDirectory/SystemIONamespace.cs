07c87ca3a6549532766b65fcc82f95db539b9a96
JuneCho23 <junecho.ucb@gmail.com>
2017-09-10 10:16:11 -0700

    File and FileInfo

--- ConsoleProject/FilesAndDirectory/SystemIONamespace.cs
+++ ConsoleProject/FilesAndDirectory/SystemIONamespace.cs
@@ -1,4 +1,15 @@
 ﻿using System;
+
+//System.IO
+
+//Common Classes
+//File, FileInfo: Provide methods for creating, copying, deleting, moving, and opening of files
+//File: Provides STATIC Methods
+//FileInfo: Provides INSTANCE Methods
+
+//Directory, DirectoyInfo
+//Path
+
 namespace ConsoleProject.FilesAndDirectory
 {
     public class SystemIONamespace
