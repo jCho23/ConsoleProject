514a66112b63bd3ce60ff721c5f23312374c87a9
JuneCho23 <junecho.ucb@gmail.com>
2017-09-09 18:40:51 -0700

    CleanUp

--- ConsoleProject/Texts/ProceduralProgramming.cs
+++ ConsoleProject/Texts/ProceduralProgramming.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+
+//Procedural Programming: A programming paradigm based on procedure calls
+//Object-Oriented Programming: A programming based on objects
+
 namespace ConsoleProject.Texts
 {
-    public class ProceduralProgramming
+    ////First Demo
+    //internal class ProceduralProgramming
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("What is your name? ");
+    //        var name = Console.ReadLine();
+
+    //        var reversed = ReverseName(name);
+    //        Console.WriteLine("Reversed Name: " + reversed);
+    //    }
+
+    //    //Here all the processing is done by the String "name" thus we passed it here
+    //    public static string ReverseName(string name)
+    //    {
+    //        var array = new char[name.Length];
+    //        for (var i = name.Length; i > 0; i--)
+    //            array[name.Length - i] = name[i - 1];
+
+    //        //var reversed = new string(array);
+
+    //        //return reversed;
+
+    //        //This code is the same as above
+    //        //Since the name of this Method (ReverseName) indicates what it is doing,
+    //        //So we don't need to store it in a seperate variable like the code above
+    //        //Here, the code is much easier to read
+    //        return new string(array);
+    //    }
+    //}
+
+
+    //Demo 2
+    internal class ProceduralProgramming
     {
-        public ProceduralProgramming()
-        {
-        }
+
+
     }
+
 }
+
