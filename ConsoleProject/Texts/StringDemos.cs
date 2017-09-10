96f6237f537dbc7ec5bb5d5cf68ec5e784d96a58
JuneCho23 <junecho.ucb@gmail.com>
2017-09-09 17:51:02 -0700

    finished Demo1

--- ConsoleProject/Texts/StringDemos.cs
+++ ConsoleProject/Texts/StringDemos.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace ConsoleProject.Texts
 {
     public class StringDemos
     {
-        public StringDemos()
-        {
-        }
+        //Demo 1
+        //Ask the user to enter a few number seperated by a hyphen
+        //Work out if the numbers are consecutive
+        //If consecutive == Display "Consecutive"
+        //Else Display Non-Consecutive
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter a few numbers seperated by a hyphen (i.e. 1-2-3-4) ");
+        //    var input = Console.ReadLine();
+
+        //    var numbers = new List<int>();
+        //    foreach (var number in input.Split('-'))
+        //        numbers.Add(Convert.ToInt32(number));
+
+        //    numbers.Sort();
+
+        //    var isConsecutive = true;
+        //    for (var i = 1; i < numbers.Count; i++)
+        //    {
+        //        if (numbers[i] !=numbers[i-1] +1)
+        //        {
+        //            isConsecutive = false;
+        //            break;
+        //        }
+        //    }
+
+        //    var message = isConsecutive ? "Consecutive" : "Not Consecutive";
+        //    Console.WriteLine(message);
+
+        //}
+
+
+        //Demo 2
+
+
+
     }
 }
